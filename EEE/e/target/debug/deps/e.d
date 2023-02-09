f97a48359c5f24615edc2a230a2e7bdae14f40da
heart <2105026385@qq.com>
2023-02-09 16:22:54 +0800

    测试E分支,添加cargo new

---
+++ EEE/e/target/debug/deps/e.d
@@ -0,0 +1,5 @@
+E:\cs\a\eee\e\target\debug\deps\e.exe: src\main.rs
+
+E:\cs\a\eee\e\target\debug\deps\e.d: src\main.rs
+
+src\main.rs:
---
+++ EEE/e/target/debug/e.d
@@ -0,0 +1,1 @@
+E:\cs\a\eee\e\target\debug\e.exe: E:\cs\a\eee\e\src\main.rs
